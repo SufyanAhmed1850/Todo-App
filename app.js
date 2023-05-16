3b99061df60b09246e0544f501c0e5c46e71fa3b
Sufyan Ahmed <SufyanAhmed1850>
2023-05-16 05:52:40 -0700

    added function to set/get todo to/from local storage

--- app.js
+++ app.js
@@ -27,74 +27,91 @@ var completedTodo = []
 // -----------------------------------------------------------------------------Set Old Todo If Todo Is Null
 var oldTodo = ""
 // -----------------------------------------------------------------------------Creates A TodoItem---------------------------------------------------------------------------//
-function setTodo() {
-    if (!userTodo.value) {
+function setTodo(inputLocal) {
+    showAll()
+    if (inputLocal || (!inputLocal && userTodo.value)) {
+        // -------------------------------------------------------------------------Create Parent Todo
+        var todo = document.createElement('div')
+        // -------------------------------------------------------------------------Create Parent Raido
+        var radioP = document.createElement('div')
+        // -------------------------------------------------------------------------Create Raido
+        var radio = document.createElement('div')
+        // -------------------------------------------------------------------------Create Input Field
+        var input = document.createElement('input')
+        // -------------------------------------------------------------------------Create Parent Img Btn
+        var edits = document.createElement('div')
+        // -------------------------------------------------------------------------Create Img Pencil
+        var img1 = document.createElement('img')
+        // -------------------------------------------------------------------------Create Img Cross
+        var img2 = document.createElement('img')
+        // -------------------------------------------------------------------------Set Parent Todo Attributes
+        todo.setAttribute('id', 'todo')
+        todo.setAttribute('class', 'todo')
+        todo.setAttribute('onmouseover', "changesBtn(this, 'over')")
+        todo.setAttribute('onmouseout', "changesBtn(this, 'out')")
+        // -------------------------------------------------------------------------Set Radio Attributes
+        radio.setAttribute('class', 'radio')
+        radio.setAttribute('onclick', 'toggleRadio(this)')
+        // -------------------------------------------------------------------------Set Input Field Attributes
+        input.setAttribute('id', 'todoItem')
+        input.setAttribute('class', 'todoItem')
+        input.setAttribute('type', 'text')
+        input.setAttribute('disabled', 'true')
+        if (inputLocal) {
+            input.setAttribute('value', inputLocal)
+            var val = 'local'
+        } else {
+            input.setAttribute('value', userTodo.value)
+            var val = 'user'
+        }
+        input.setAttribute('onblur', "diableField(this)")
+        input.setAttribute('autocomplete', "off")
+        input.setAttribute('spellcheck', "false")
+        // -------------------------------------------------------------------------Set Parent Img Attributes
+        edits.setAttribute('id', 'edits')
+        edits.setAttribute('class', 'edits')
+        // -------------------------------------------------------------------------Set Pencil Attributes
+        img1.setAttribute('id', 'editBtn')
+        img1.setAttribute('src', './images/icon-pencil.svg')
+        img1.setAttribute('width', '20px')
+        img1.setAttribute('onclick', 'runEdit(this)')
+        // -------------------------------------------------------------------------Set Cross Attributes
+        img2.setAttribute('id', 'delBtn')
+        img2.setAttribute('src', './images/icon-cross.svg')
+        img2.setAttribute('width', '26px')
+        img2.setAttribute('onclick', 'del(this)')
+        // -------------------------------------------------------------------------Append Radio To RadioP
+        radioP.appendChild(radio)
+        // -------------------------------------------------------------------------Append Img1 To edits
+        edits.appendChild(img1)
+        // -------------------------------------------------------------------------Append Img2 To edits
+        edits.appendChild(img2)
+        // -------------------------------------------------------------------------Append radioP To Parent (Todo)
+        todo.appendChild(radioP)
+        // -------------------------------------------------------------------------Append Input To Parent (Todo)
+        todo.appendChild(input)
+        // -------------------------------------------------------------------------Append Edits To Parent (Todo)
+        todo.appendChild(edits)
+        // -------------------------------------------------------------------------Reset Value Of UserInput
+        userTodo.value = ""
+        // -------------------------------------------------------------------------Append Parent (Todo) To TodoContainer
+        todoContainer.insertBefore(todo, todoContainer.firstChild)
+        getActiveAndCompleted()
+        // -------------------------------------------------------------------------Set Length Of TodoList
+        leftItems.innerHTML = activeTodo.length + ' items left'
+        // -------------------------------------------------------------------------Calls Function That Sets todoItem To Local
+        switch (val) {
+            case 'user':
+                toLocal()
+                console.log('user')
+                break
+            default:
+                console.log('local')
+                break
+        }
+    } else if (!userTodo.value) {
         return
     }
-    showAll()
-    // -------------------------------------------------------------------------Create Parent Todo
-    var todo = document.createElement('div')
-    // -------------------------------------------------------------------------Create Parent Raido
-    var radioP = document.createElement('div')
-    // -------------------------------------------------------------------------Create Raido
-    var radio = document.createElement('div')
-    // -------------------------------------------------------------------------Create Input Field
-    var input = document.createElement('input')
-    // -------------------------------------------------------------------------Create Parent Img Btn
-    var edits = document.createElement('div')
-    // -------------------------------------------------------------------------Create Img Pencil
-    var img1 = document.createElement('img')
-    // -------------------------------------------------------------------------Create Img Cross
-    var img2 = document.createElement('img')
-    // -------------------------------------------------------------------------Set Parent Todo Attributes
-    todo.setAttribute('id', 'todo')
-    todo.setAttribute('class', 'todo')
-    todo.setAttribute('onmouseover', "changesBtn(this, 'over')")
-    todo.setAttribute('onmouseout', "changesBtn(this, 'out')")
-    // -------------------------------------------------------------------------Set Radio Attributes
-    radio.setAttribute('class', 'radio')
-    radio.setAttribute('onclick', 'toggleRadio(this)')
-    // -------------------------------------------------------------------------Set Input Field Attributes
-    input.setAttribute('id', 'todoItem')
-    input.setAttribute('class', 'todoItem')
-    input.setAttribute('type', 'text')
-    input.setAttribute('disabled', 'true')
-    input.setAttribute('value', userTodo.value)
-    input.setAttribute('onblur', "diableField(this)")
-    input.setAttribute('autocomplete', "off")
-    input.setAttribute('spellcheck', "false")
-    // -------------------------------------------------------------------------Set Parent Img Attributes
-    edits.setAttribute('id', 'edits')
-    edits.setAttribute('class', 'edits')
-    // -------------------------------------------------------------------------Set Pencil Attributes
-    img1.setAttribute('id', 'editBtn')
-    img1.setAttribute('src', './images/icon-pencil.svg')
-    img1.setAttribute('width', '20px')
-    img1.setAttribute('onclick', 'runEdit(this)')
-    // -------------------------------------------------------------------------Set Cross Attributes
-    img2.setAttribute('id', 'delBtn')
-    img2.setAttribute('src', './images/icon-cross.svg')
-    img2.setAttribute('width', '26px')
-    img2.setAttribute('onclick', 'del(this)')
-    // -------------------------------------------------------------------------Append Radio To RadioP
-    radioP.appendChild(radio)
-    // -------------------------------------------------------------------------Append Img1 To edits
-    edits.appendChild(img1)
-    // -------------------------------------------------------------------------Append Img2 To edits
-    edits.appendChild(img2)
-    // -------------------------------------------------------------------------Append radioP To Parent (Todo)
-    todo.appendChild(radioP)
-    // -------------------------------------------------------------------------Append Input To Parent (Todo)
-    todo.appendChild(input)
-    // -------------------------------------------------------------------------Append Edits To Parent (Todo)
-    todo.appendChild(edits)
-    // -------------------------------------------------------------------------Reset Value Of UserInput
-    userTodo.value = ""
-    // -------------------------------------------------------------------------Append Parent (Todo) To TodoContainer
-    todoContainer.insertBefore(todo, todoContainer.firstChild)
-    getActiveAndCompleted()
-    // -------------------------------------------------------------------------Set Length Of TodoList
-    leftItems.innerHTML = activeTodo.length + ' items left'
 }
 // -----------------------------------------------------------------------------Calls setTodo Function On Click
 submit.addEventListener('click', setTodo)
@@ -187,6 +204,8 @@ function del(element) {
     getActiveAndCompleted()
     // -------------------------------------------------------------------------Updates Length Of TodoList
     leftItems.innerHTML = activeTodo.length + ' items left'
+    // -------------------------------------------------------------------------Calls Function That Sets todoItem To Local
+    toLocal()
 }
 // -----------------------------------------------------------------------------Adds Checked Class On Radio------------------------------------------------------------------//
 function toggleRadio(element) {
@@ -207,6 +226,8 @@ function toggleRadio(element) {
     getActiveAndCompleted()
     // -------------------------------------------------------------------------Updates Length Of TodoList
     leftItems.innerHTML = activeTodo.length + ' items left'
+    // -------------------------------------------------------------------------Calls Function That Sets todoItem To Local
+    toLocal()
 }
 // -----------------------------------------------------------------------------Deletes Completed Todo Items-----------------------------------------------------------------//
 function delCompleted() {
@@ -216,38 +237,40 @@ function delCompleted() {
     // -------------------------------------------------------------------------Updates Length Of TodoList
     leftItems.innerHTML = activeTodo.length + ' items left'
     getActiveAndCompleted()
+    // -------------------------------------------------------------------------Calls Function That Sets todoItem To Local
+    toLocal()
 }
 // -----------------------------------------------------------------------------Shows Only Completed TodoItems---------------------------------------------------------------//
 function showCompleted() {
-        var all = document.getElementById('all')
-        var active = document.getElementById('active')
-        var completed = document.getElementById('completed')
-        all.classList.remove('focus')
-        active.classList.remove('focus')
-        completed.classList.add('focus')
-        todoContainer.innerHTML = ''
-        for (var i = 0; i < completedTodo.length; i++) {
-            var completedTodoParent = completedTodo[i].parentNode
-            todoContainer.appendChild(completedTodoParent)
-        }
-        // -------------------------------------------------------------------------Updates Length Of TodoList
-        leftItems.innerHTML = activeTodo.length + ' items left'
+    var all = document.getElementById('all')
+    var active = document.getElementById('active')
+    var completed = document.getElementById('completed')
+    all.classList.remove('focus')
+    active.classList.remove('focus')
+    completed.classList.add('focus')
+    todoContainer.innerHTML = ''
+    for (var i = 0; i < completedTodo.length; i++) {
+        var completedTodoParent = completedTodo[i].parentNode
+        todoContainer.appendChild(completedTodoParent)
+    }
+    // -------------------------------------------------------------------------Updates Length Of TodoList
+    leftItems.innerHTML = activeTodo.length + ' items left'
 }
 // -----------------------------------------------------------------------------Shows Only Active TodoItems------------------------------------------------------------------//
 function showActive() {
-        var all = document.getElementById('all')
-        var active = document.getElementById('active')
-        var completed = document.getElementById('completed')
-        all.classList.remove('focus')
-        active.classList.add('focus')
-        completed.classList.remove('focus')
-        todoContainer.innerHTML = ''
-        for (var i = 0; i < activeTodo.length; i++) {
-            var activeTodoParent = activeTodo[i].parentNode
-            todoContainer.appendChild(activeTodoParent)
-        }
-        // -------------------------------------------------------------------------Updates Length Of TodoList
-        leftItems.innerHTML = activeTodo.length + ' items left'
+    var all = document.getElementById('all')
+    var active = document.getElementById('active')
+    var completed = document.getElementById('completed')
+    all.classList.remove('focus')
+    active.classList.add('focus')
+    completed.classList.remove('focus')
+    todoContainer.innerHTML = ''
+    for (var i = 0; i < activeTodo.length; i++) {
+        var activeTodoParent = activeTodo[i].parentNode
+        todoContainer.appendChild(activeTodoParent)
+    }
+    // -------------------------------------------------------------------------Updates Length Of TodoList
+    leftItems.innerHTML = activeTodo.length + ' items left'
 }
 // -----------------------------------------------------------------------------Shows All TodoItems--------------------------------------------------------------------------//
 function showAll() {
@@ -265,3 +288,25 @@ function showAll() {
     // -------------------------------------------------------------------------Updates Length Of TodoList
     leftItems.innerHTML = activeTodo.length + ' items left'
 }
+// -----------------------------------------------------------------------------Sets todoItems to Local Storage---------------------------------------------------------------//
+function toLocal() {
+    var childTodoItems = todoContainer.querySelectorAll('.todoItem')
+    var childTodoItemsArr = [...childTodoItems]
+    var childTodoValueArr = []
+    for (var i = 0; i < childTodoItemsArr.length; i++) {
+        childTodoValueArr.unshift(childTodoItemsArr[i].value)
+    }
+    var strChildTodoItemsArr = JSON.stringify(childTodoValueArr)
+    localStorage.setItem('localTodoItems', strChildTodoItemsArr)
+}
+// -----------------------------------------------------------------------------Gets todoItems From Local Storage---------------------------------------------------------------//
+function getLocal() {
+    var getFromLocal = localStorage.getItem('localTodoItems')
+    var localParse = JSON.parse(getFromLocal)
+    // -------------------------------------------------------------------------If Local Have todoItemsValue Then It Append todoItem with Value From Local
+    if (localParse) {
+        for (var i = 0; i <= localParse.length; i++) {
+            setTodo(localParse[i])
+        }
+    }
+} getLocal()
